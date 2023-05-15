fcf73de9cfcd01acb4fbcb5d08b0f5c17d40168d
Manh ND <manhnd093@gmail.com>
2023-05-16 03:50:06 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,151 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'firebase_options.dart';
+import 'screen/screen1.dart';
+import 'screen/screen2.dart';
+import 'screen/screen3.dart';
+import 'screen/screen4.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    init();
+    // TODO: implement initState
+    super.initState();
+  }
+
+  final List<Widget> _widgetOptions = <Widget>[
+    Screen1(link: "",),
+    Screen2(link: ""),
+    Screen3(link: ""),
+    Screen4(link: ""),
+  ];
+
+  final List<String> _icon = [
+    'assets/icons/house-solid.svg',
+    'assets/icons/right-to-bracket-solid.svg',
+    'assets/icons/right-from-bracket-solid.svg',
+    'assets/icons/circle-info-solid.svg',
+  ];
+  void init() async {}
+
+  void _onItemTapped(int index) async {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    Navigator.pop(context);
+  }
+
+  final List<String> _title = ['Trang chủ', 'Đăng nhập', 'Đăng ký', 'Hỗ trợ'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: const Color(0xffD0BCFF),
+          // backgroundColor: backgroundColor,
+          iconTheme: const IconThemeData(color: Color(0xff21005D)),
+          title: Text(
+            _title[_selectedIndex],
+            style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+                color: Color(0xff21005D)),
+          ),
+          elevation: 0,
+        ),
+        // backgroundColor: backgroundColor,
+        drawer: Drawer(
+          backgroundColor: Colors.white,
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              SizedBox(
+                height: 112,
+                child: DrawerHeader(
+                    decoration: const BoxDecoration(
+                      color: Color(0xffD0BCFF),
+                    ),
+                    child: Row(
+                      children: const [],
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  children: List.generate(
+                      4, (index) {
+                    return ListTile(
+                      leading: SvgPicture.asset(
+                        _icon[index],
+                        color: index == _selectedIndex
+                            ? const Color(0xffD0BCFF)
+                            : const Color(0xff21005D),
+                        width: 23,
+                      ),
+                      title: Text(
+                        _title[index],
+                        // style: TextStyle(color: Colors.white),
+                        style: index != _selectedIndex
+                            ? const TextStyle(
+                            height: 0, fontSize: 14, color: Colors.grey)
+                            : const TextStyle(
+                            height: 0, fontSize: 14, color: Color(0xffD0BCFF)),
+                      ),
+                      selected: _selectedIndex == index,
+                      onTap: () async {
+                        _onItemTapped(index);
+                        // }
+                      },
+                    );
+                  }
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        body: _widgetOptions.elementAt(_selectedIndex));
+  }
+}
---
+++ lib/screen/screen4.dart
@@ -0,0 +1,119 @@
+import 'dart:ui';
+
+import 'package:firebase_remote_config/firebase_remote_config.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+// Import for iOS features.
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+
+class Screen4 extends StatefulWidget {
+  Screen4({super.key, required this.link});
+  String link;
+  @override
+  State<Screen4> createState() => _Screen4State();
+}
+
+class _Screen4State extends State<Screen4> {
+  late final WebViewController _controller;
+  final _remoteConfig = FirebaseRemoteConfig.instance;
+  @override
+  void initState() {
+    // TODO: implement initState
+    // init();
+    super.initState();
+    widget.link = _remoteConfig.getString('weblink4');
+    late final PlatformWebViewControllerCreationParams params;
+    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
+      params = WebKitWebViewControllerCreationParams(
+        allowsInlineMediaPlayback: true,
+        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
+      );
+    } else {
+      params = const PlatformWebViewControllerCreationParams();
+    }
+
+    final WebViewController controller =
+        WebViewController.fromPlatformCreationParams(params);
+    // #enddocregion platform_features
+
+    controller
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            debugPrint('WebView is loading (progress : $progress%)');
+          },
+          onPageStarted: (String url) {
+            debugPrint('Page started loading: $url');
+          },
+          onPageFinished: (String url) {
+            debugPrint('Page finished loading: $url');
+          },
+          onWebResourceError: (WebResourceError error) {
+            debugPrint('''
+            Page resource error:
+              code: ${error.errorCode}
+              description: ${error.description}
+              errorType: ${error.errorType}
+              isForMainFrame: ${error.isForMainFrame}
+          ''');
+          },
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              debugPrint('blocking navigation to ${request.url}');
+              return NavigationDecision.prevent;
+            }
+            debugPrint('allowing navigation to ${request.url}');
+            return NavigationDecision.navigate;
+          },
+          onUrlChange: (UrlChange change) {
+            debugPrint('url change to ${change.url}');
+          },
+        ),
+      )
+      ..addJavaScriptChannel(
+        'Toaster',
+        onMessageReceived: (JavaScriptMessage message) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(message.message)),
+          );
+        },
+      )
+      ..loadRequest(Uri.parse(widget.link));
+
+    // #docregion platform_features
+    if (controller.platform is AndroidWebViewController) {
+      AndroidWebViewController.enableDebugging(true);
+      (controller.platform as AndroidWebViewController)
+          .setMediaPlaybackRequiresUserGesture(false);
+    }
+    // #enddocregion platform_features
+
+    _controller = controller;
+  }
+
+  init() async {
+    await _remoteConfig.setConfigSettings(RemoteConfigSettings(
+      fetchTimeout: const Duration(seconds: 60),
+      minimumFetchInterval: const Duration(seconds: 1),
+    ));
+    var web1 = "";
+    await _remoteConfig.ensureInitialized();
+    await _remoteConfig.activate();
+    await _remoteConfig.fetchAndActivate().then((value) {
+      web1 = _remoteConfig.getString('weblink1');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(title: const Text('Flutter Simple Example')),
+      body: WebViewWidget(controller: _controller),
+    );
+  }
+}
